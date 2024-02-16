73573a1fede8599e1be90eaae5fefecef44e82d2
abc <abc@gmail.com>
2024-02-16 09:32:18 +0530

    all files

---
+++ NumberSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsEx
+{
+    public class NumberSystem
+    {
+        static int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        public static void OddNum()
+        {
+            foreach(int i in numbers)
+            {
+                if(i%2==1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        public static void EvenNum()
+        {
+            foreach(int i in numbers) 
+            {
+                if(i%2==0) 
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using JenkinsEx;
+
+Console.WriteLine("welcome to number system");
+Console.WriteLine("*****Even numbers******");
+NumberSystem.EvenNum();
+Console.WriteLine("*****Odd numbers*******");
+NumberSystem.OddNum();
